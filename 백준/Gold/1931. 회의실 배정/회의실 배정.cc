51542166bc26f536f7e72c4239256fa0bcb811ad
JhiWhoon Kang <109329462+JhiWhoonKang@users.noreply.github.com>
2025-03-13 16:43:04 +0900

    [Gold V] Title: 회의실 배정, Time: 24 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -1,40 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main(void)
+{
+	cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(false);
 
-    int N;
-    cin >> N;
-    vector<pair<int, int>> meetings(N); // pair: {끝나는 시간, 시작 시간}
+	int N;		// 회의 개수
+	cin >> N;
 
-    for (int i = 0; i < N; i++){
-        int start, end;
-        cin >> start >> end;
-        meetings[i] = {end, start};
-    }
+	vector<pair<int, int>>conference(N);	// <startTime, endTime>
+	for (int i = 0; i < N; ++i)
+	{
+		int startTime, endTime;
+		cin >> startTime >> endTime;
+		conference[i] = { startTime, endTime };
+	}
 
-    // 종료시간 기준으로 오름차순 정렬, 종료시간이 같다면 시작시간 기준으로 정렬
-    sort(meetings.begin(), meetings.end(), [](const pair<int, int>& a, const pair<int, int>& b){
-        if(a.first == b.first)
-            return a.second < b.second;
-        return a.first < b.first;
-    });
+    sort(conference.begin(), conference.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+        if (a.second == b.second)
+            return a.first < b.first;
+        return a.second < b.second;
+        });
 
-    int count = 0;
-    int last_end = 0; // 회의 시간이 0부터 시작할 수 있으므로 적절한 초기값 설정
-    for (auto &meeting : meetings) {
-        int end = meeting.first;
-        int start = meeting.second;
-        if (start >= last_end) {  // 회의가 이전 회의 종료시간 이후에 시작하면 선택
-            count++;
-            last_end = end;
+    int cnt = 0;
+    int prev_end_time = 0;
+    for (int i = 0; i < N; i++) 
+    {
+        // 현재 회의의 시작 시간이 마지막 회의의 종료 시간보다 크거나 같으면 선택
+        if (conference[i].first >= prev_end_time) 
+        {
+            ++cnt;
+            prev_end_time = conference[i].second;
         }
     }
 
-    cout << count << "\n";
-    return 0;
+    cout << cnt << "\n";
+
+	return 0;
 }
