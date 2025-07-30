397cc61f7a169930fa810d52f0bdb95ed24ed6c1
JhiWhoon Kang <109329462+JhiWhoonKang@users.noreply.github.com>
2025-07-30 16:56:58 +0900

    [level 3] Title: 불량 사용자, Time: 39.44 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool findId(const string& user, const string& banned)
+{
+    if (user.size() != banned.size()) return false;
+    for (int i = 0; i < user.size(); i++) {
+        if (banned[i] == '*') continue;
+        if (user[i] != banned[i]) return false;
+    }
+    return true;
+}
+
+void dfs(int idx, const vector<string>& userList, const vector<string>& bannedList, set<string>& selected, set<set<string>>& allCases)
+{
+    if(idx == bannedList.size())
+    {
+        allCases.insert(selected);
+        return;
+    }
+
+    for(int i = 0; i < userList.size(); i++)
+    {
+        const string& user = userList[i];
+        if(selected.find(user) != selected.end())
+        {
+            continue;
+        }
+
+        if(findId(user, bannedList[idx]))
+        {
+            selected.insert(user);
+            dfs(idx + 1, userList, bannedList, selected, allCases);
+            selected.erase(user);
+        }
+    }
+}
+
+int solution(vector<string> user_id, vector<string> banned_id) {
+    set<set<string>> allCases;
+    set<string> selected;
+    dfs(0, user_id, banned_id, selected, allCases);
+    return allCases.size();
+}
